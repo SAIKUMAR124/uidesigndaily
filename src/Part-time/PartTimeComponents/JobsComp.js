cc45ce6c6383bd21bfdcd7bb3174aac64035497e
dev global <pavankumar.s12459@gmail.com>
2022-02-20 14:50:42 +0530

    Added colors to the Jobs

--- src/Part-time/PartTimeComponents/JobsComp.js
+++ src/Part-time/PartTimeComponents/JobsComp.js
@@ -7,7 +7,9 @@ const JobsComp = ({ search, data }) => {
         .filter((d) => d.job_name.toLowerCase().includes(search.toLowerCase()))
         .map((job) => {
           return (
-            <div key={job.id} className="jobs-container">
+            <div key={job.id} className="jobs-container" 
+              style={{backgroundColor: `${job.color}`}}
+            >
               <div>{job.job_image}</div>
               <div>{job.job_name}</div>
             </div>
--- src/Part-time/PartTimeData/data.js
+++ src/Part-time/PartTimeData/data.js
@@ -3,40 +3,48 @@ export const jobs = [
         id: 0,
         job_name: "Furniture",
         job_image: "",
+        color: "#7ff59f",
     },
     {
         id: 1,
         job_name: "Property",
         job_image: "",
+        color: "#e7eda6",
     },
     {
         id: 2,
         job_name: "Job",
         job_image: "",
+        color: "#eb8375",
     },
     {
         id: 3,
         job_name: "Travel",
         job_image: "",
+        color: "#9af5ec",
     },
     {
         id: 4,
         job_name: "Boat",
         job_image: "",
+        color: "#79b7e0",
     },
     {
         id: 5,
         job_name: "MC",
         job_image: "",
+        color: "#8979e0",
     },
     {
         id: 6,
         job_name: "Economy",
         job_image: "",
+        color: "#ed82dd",
     },
     {
         id: 7,
         job_name: "Holiday",
         job_image: "",
+        color: "#e34060",
     },
 ]
