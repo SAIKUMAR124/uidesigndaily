bad1eac901ef9e00040eb206ca57ba06ac11169e
dev global <pavankumar.s12459@gmail.com>
2022-03-01 19:19:39 +0530

    items Completed Added

--- src/Groceries/Components/GroceriesCompleted.js
+++ src/Groceries/Components/GroceriesCompleted.js
@@ -1,17 +1,37 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
+import React from "react";
+import { useSelector } from "react-redux";
+import { AiOutlineCheckCircle } from "react-icons/ai";
 
 const GroceriesCompleted = () => {
-    const itemsDone = useSelector((state)=> state.grocery.itemsCompleted);
+  const itemsDone = useSelector((state) => state.grocery.itemsCompleted);
 
   return (
     <div>
-        <div>Groceries Completed</div>
-        <div>{itemsDone.map((item)=>{
-            return <div key={item.id}>{item.name}</div>
-        })}</div>
+      {itemsDone.length > 0 ? (
+        <div>
+          <div>Groceries Completed</div>
+          <div>
+            {itemsDone.map((item) => {
+              return (
+                <div
+                  key={item.id}
+                  className="gro-item-btn"
+                  style={{ color: "green" }}
+                >
+                  <div className="radio-icon">
+                    <AiOutlineCheckCircle />
+                  </div>
+                  <div className="item-text">{item.name}</div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      ) : (
+        <div>No items completed</div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default GroceriesCompleted
+export default GroceriesCompleted;
